f6c6609f70f0d22123863a3ecbdd0309c3f78598
siyatut <anastasiya.tyutinova@gmail.com>
2025-01-30 16:19:12 +0300

    Implement TaskListView UI layout

---
+++ ToDoListTests/ToDoListTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListTests.swift
+//  ToDoListTests
+//
+//  Created by Anastasia Tyutinova on 30/1/2568 BE.
+//
+
+import Testing
+@testable import ToDoList
+
+struct ToDoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
